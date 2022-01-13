bce5adf7be13d0ce4f3d36c87f6e5063f3ef2fce
wb1110 <strengthcoachwb@gmail.com>
2022-01-13 12:52:17 -0600

    endpoints

--- server.js
+++ server.js
@@ -3,4 +3,16 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+server.get('/api/users', (req, res) => {
+  res.send('hello users');
+});
+
+server.get('/api/register', (req, res) => {
+  res.send('hello register');
+});
+
+server.get('/api/login', (req, res) => {
+  res.send('hello login');
+});
+
 module.exports = server;
