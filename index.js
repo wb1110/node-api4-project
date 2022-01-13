cc8d0510a7ba49443c7398ffc062b90877e21da5
wb1110 <strengthcoachwb@gmail.com>
2022-01-13 12:14:23 -0600

    ready to deploy2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('./server');
 require('dotenv').config();
 
-const port = 4000;
+const port = process.env.PORT || 9000;
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
